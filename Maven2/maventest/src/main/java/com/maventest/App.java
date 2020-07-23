e75b7778c5064bbcc4d1181da80eb9bc08324c30
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-07-23 16:50:26 -0300

    Test Junit and Java on VSCode

---
+++ Maven2/maventest/src/main/java/com/maventest/App.java
@@ -0,0 +1,14 @@
+package com.maventest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( final String[] args)
+    {
+        String x = "Felipe Brigo, Hello world!!";
+        System.out.println( x );
+    }
+}
